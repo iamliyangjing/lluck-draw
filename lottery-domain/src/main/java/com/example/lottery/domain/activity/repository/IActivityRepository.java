61a9a218ae7db6e68c492c7234cab0a2bfaa3dfd
18381826414 <liyl778@163.com>
2023-05-26 01:00:11 +0800

    完成VO包装类

---
+++ lottery-domain/src/main/java/com/example/lottery/domain/activity/repository/IActivityRepository.java
@@ -0,0 +1,10 @@
+package com.example.lottery.domain.activity.repository;
+
+/**
+ * @program: lluck-draw
+ * @description:
+ * @author: lyj
+ * @create: 2023-05-26 00:50
+ **/
+public interface IActivityRepository {
+}
